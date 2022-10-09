4c8880df1f7f3eec8074fb20d773dc82e539f9df
Abdelrahman Youssef <ayouss@uwo.ca>
2022-10-09 16:21:32 -0400

    Added more commands

--- bot.py
+++ bot.py
@@ -56,7 +56,7 @@ async def cavalry(ctx):
     await ctx.respond(embed)
 
 @bot.command
-@lightbulb.command('xy', 'Send the Best Cavalry Equipment', aliases=['Xy, Xiang Yu, xiang yu', 'XY'])
+@lightbulb.command('xy', 'Xiang Yu Talent Builds', aliases=['Xy', 'Xiang', 'xiang', 'XY'])
 @lightbulb.implements(lightbulb.PrefixCommand)
 async def xy(ctx):
     embed1 = (
@@ -71,5 +71,78 @@ async def xy(ctx):
     .set_footer("Brought to you by kingdom 2545")
     )
     await ctx.respond(embed2)
+
+@bot.command
+@lightbulb.command('zeno', 'Zenobia Talent Builds', aliases=['Zeno', 'Zenobia', 'zenobia'])
+@lightbulb.implements(lightbulb.PrefixCommand)
+async def zeno(ctx):
+    embed = (
+    hikari.Embed(title="Zenobia", description="Zenobia garrison build")
+    .set_image(hikari.File('images/Zenobia_Garrison.png'))
+    .set_footer("Brought to you by kingdom 2545")
+    )
+    await ctx.respond(embed)
+
+@bot.command
+@lightbulb.command('nevsky', 'Alexander Nevsky Talent Builds', aliases=['Nev', 'Nevsky', 'nev'])
+@lightbulb.implements(lightbulb.PrefixCommand)
+async def nev(ctx):
+    embed = (
+    hikari.Embed(title="Alexander Nevsky", description="Nevsky universal build")
+    .set_image(hikari.File('images/Nevsky.png'))
+    .set_footer("Brought to you by kingdom 2545")
+    )
+    await ctx.respond(embed)
+
+@bot.command
+@lightbulb.command('freddie', 'Frederick Talent Builds', aliases=['Frederick', 'Freddie', 'Fred', 'fred', 'frederick'])
+@lightbulb.implements(lightbulb.PrefixCommand)
+async def fred(ctx):
+    embed = (
+    hikari.Embed(title="Frederick", description="Frederick mixed troops rally build")
+    .set_image(hikari.File('images/Frederick_MixTroopsRally.png'))
+    .set_footer("Brought to you by kingdom 2545")
+    )
+    await ctx.respond(embed)
+
+@bot.command
+@lightbulb.command('ragnar', 'Ragnar Talent Builds', aliases=['Ragnar'])
+@lightbulb.implements(lightbulb.PrefixCommand)
+async def ragnar(ctx):
+    embed = (
+    hikari.Embed(title="Ragnar", description="Ragnar mixed troops rally build")
+    .set_image(hikari.File('images/Ragnar_Rally.png'))
+    .set_footer("Brought to you by kingdom 2545")
+    )
+    await ctx.respond(embed)
+
+@bot.command
+@lightbulb.command('aethel', 'Aethelflaed Talent Builds', aliases=['Aethel', 'Aethelflaed', 'aethelflaed'])
+@lightbulb.implements(lightbulb.PrefixCommand)
+async def aethel(ctx):
+    embed = (
+    hikari.Embed(title="Aethelflaed", description="Aethelflaed mixed troops openfield")
+    .set_image(hikari.File('images/Aethelflaed_FieldMixTroops.png'))
+    .set_footer("Brought to you by kingdom 2545")
+    )
+    await ctx.respond(embed)
+
+@bot.command
+@lightbulb.command('Alex', 'Alexander The Great Talent Builds', aliases=['alex', 'Alexander', 'alexander'])
+@lightbulb.implements(lightbulb.PrefixCommand)
+async def alex(ctx):
+    embed1 = (
+    hikari.Embed(title="Alexander The Great", description="Alex no skill damage build")
+    .set_image(hikari.File('images/Alexander_FieldNoSkillDMG.png'))
+    .set_footer("Brought to you by kingdom 2545")
+    )
+    await ctx.respond(embed1)
+    embed2 = (
+    hikari.Embed(title="Alexander The Great", description="Alex skill damage build")
+    .set_image(hikari.File('images/Alexander_FieldRallySkillDMG.png'))
+    .set_footer("Brought to you by kingdom 2545")
+    )
+    await ctx.respond(embed2)
+
 
 bot.run()
